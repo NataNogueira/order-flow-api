84253b28b71b8b027fd4d50855ec3c4c58b694ef
Natã Nogueira <natanogueiraskt@gmail.com>
2026-03-09 06:03:39 -0300

    feat: adiciona arquivo de entrada principal da aplicacao

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// Instanciamento
+const express = require('express');
+const orderRoutes = require('./routes/orderRoutes');
+
+const app = express();
+
+app.use(express.json());
+
+// Pluga as rotas
+app.use('/order', orderRoutes);
+
+module.exports = app; // Exporta o app configurado
